5ec9cdf26b926687095480303cd3b55dd6198d2a
tkjen <thienngo2003@gmail.com>
2025-05-23 15:34:42 +0700

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -33,8 +35,15 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    viewBinding {
+        enable = true
+    }
 }
-
+val lifecycle_version = "2.9.0"
+val arch_version = "2.2.0"
+val fragment_version = "1.8.6"
+val room_version = "2.7.1"
+val hilt_version = "2.51.1" // Sửa phiên bản
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -45,4 +54,23 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Lifecycles only (without ViewModel or LiveData)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
+    //fragment
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+
+    // Hilt (Dùng KSP)
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    ksp("com.google.dagger:hilt-android-compiler:$hilt_version")
 }
---
+++ app/src/main/java/com/tkjen/weather/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.tkjen.weather
+
+import android.app.Application
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Các cấu hình khác nếu cần
+    }
+}
